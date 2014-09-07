99953ff61b0ea320c466322abc51d6e05f8cce6e
“pirinvk” <pirindroid@gmail.com>
2014-09-07 13:05:49 +0200

    add binary sensors

--- rockchip-common/rockchip-vendor-blobs.mk
+++ rockchip-common/rockchip-vendor-blobs.mk
@@ -57,7 +57,9 @@ PRODUCT_COPY_FILES += \
     vendor/bq/rockchip-common/proprietary/lib/hw/camera.rk30board.so:system/lib/hw/camera.rk30board.so \
     vendor/bq/rockchip-common/proprietary/lib/hw/gpu.rk30board.so:system/lib/hw/gpu.rk30board.so \
     vendor/bq/rockchip-common/proprietary/lib/hw/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so \
-    vendor/bq/rockchip-common/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so
+    vendor/bq/rockchip-common/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so \
+    vendor/bq/rockchip-common/proprietary/lib/hw/sensors.rk30board.so:system/lib/hw/sensors.rk30board.so
+
     
 # Widevine
 PRODUCT_COPY_FILES += \
